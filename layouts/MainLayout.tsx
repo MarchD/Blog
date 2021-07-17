7aefed4002b8380ac58a8f2eb757e30bae88ba5e
MarchD <dubchmar@gmail.com>
2021-07-17 10:17:02 +0300

    added globalStyle and styles of MainLayout

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Header } from '../components/Header/Header';
 import { Footer } from '../components/Footer/Footer';
+import styled from 'styled-components';
+
+const MainLayoutStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    min-height: 100vh;
+`;
 
 export const MainLayout: React.FC = ({ children }) => {
     return (
-        <div>
+        <MainLayoutStyled>
             <Header />
             {children}
             <Footer />
-        </div>
+        </MainLayoutStyled>
     );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,44 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 10px;
+  }
+
+  body {
+    font-family: Montserrat, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-size: 1.6rem;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  a {
+    color: #000;
+    text-decoration: none;
+  }
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <GlobalStyle />
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
